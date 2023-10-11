fa41043869eeccb5ec65586786466bbc2dd8ffb0
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-10-11 04:21:40 -0600

    Uploaded and updated files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Hybrid Perk Deck
+A mod that provides a variety of buffs for *PAYDAY: The Heist*.
+
+This mod is still in active development. It's unlikely to work for a little while.
+## Effects
+placeholder
+## Installation
+placeholder
---
+++ hybrid_perk_deck/base.lua
@@ -0,0 +1,14 @@
+local module = DMod:new("hybrid_perk_deck", {
+	abbr = "hybriddeck",
+	author = "questmatrix",
+	description = "https://github.com/questmatrix/hybrid-perk-deck",
+	version = "1"
+})
+
+module:hook_post_require("lib/units/beings/player/playerdamage", "playerdamage")
+
+module:hook_post_require("lib/tweak_data/upgradestweakdata", "upgradestweakdata")
+
+module:hook_post_require("lib/units/beings/player/states/playerstandard", "playerstandard")
+
+return module
